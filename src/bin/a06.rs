a5a7976e488c1a4f199edf024d7f243527caed19
operathe <operathe11@gmail.com>
2024-03-31 13:54:27 +0900

    a06 change

--- src/bin/a06.rs
+++ src/bin/a06.rs
@@ -1,28 +1,22 @@
 use proconio::{fastout, input};
 
+#[allow(non_snake_case)]
 #[fastout]
 fn main() {
     input! {
-        H: usize,
-        W: usize,
-        matrix: [[usize; H]; W],
-        Q: usize,
-        ABCD: [(usize, usize, usize, usize); Q],
-    }
-    let mut Z = vec![vec![0; W + 1]; H + 1];
-    for i in 1..=H {
-        for j in 1..=W {
-            Z[i][j] = Z[i][j - 1] + matrix[i - 1][j - 1];
-        }
-    }
-    for j in 1..=W {
-        for i in 1..=H {
-            Z[i][j] += Z[i - 1][j];
-        }
+     N: usize,
+     Q: usize,
+     A: [usize; N],
+     LR: [(usize, usize); Q]
     }
 
-    for &(a, b, c, d) in &ABCD {
-        let ans = Z[c][d] + Z[a - 1][b - 1] - Z[a - 1][d] - Z[c][b - 1];
-        println!("{}", ans);
+    let mut S = vec![0];
+
+    for i in 0..N {
+        S.push(A[i] + S[i])
+    }
+
+    for (L, R) in &LR {
+        println!("{:?}", S[*R] - S[*L - 1]);
     }
 }
